f19afb6aee3178a76bc30fd753199669ff4ef67a
suzhenyi <suzhenyi1999@163.com>
2022-08-16 20:45:18 +0800

    视频详情页

--- pages/detail-video/detail-video.js
+++ pages/detail-video/detail-video.js
@@ -1,66 +1,40 @@
 // pages/detail-video/detail-video.js
+import { getMVUrl, getMVInfo, getMVRelated } from "../../services/video"
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    id: 0,
+    mvUrl: "",
+    mvInfo: {},
+    relatedVideo: [],
+    // 弹幕数据：
+    danmuList: [
+      { text: "哈哈哈, 真好听", color: "#ff0000", time: 3 },
+      { text: "呵呵呵, 不错哦", color: "#ffff00", time: 10 },
+      { text: "嘿嘿嘿, 好喜欢", color: "#0000ff", time: 15 },
+    ]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad(options) {
+    // 1.获取id
+    const id = options.id
+    this.setData({ id })
 
+    // 2.请求数据
+    this.fetchMVUrl()
+    this.fetchMVInfo()
+    this.fetchMVRelated()
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
+  async fetchMVUrl() {
+    const res = await getMVUrl(this.data.id)
+    this.setData({ mvUrl: res.data.url })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
+  async fetchMVInfo() {
+    const res = await getMVInfo(this.data.id)
+    this.setData({ mvInfo: res.data })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+  async fetchMVRelated() {
+    const res = await getMVRelated(this.data.id)
+    this.setData({ relatedVideo: res.data })
   }
 })
