d0b03812e120d2f87788aab46916b6f096b665a3
suzhenyi <suzhenyi1999@163.com>
2022-08-25 23:57:06 +0800

    图片预览

--- pages/music-player/music-player.js
+++ pages/music-player/music-player.js
@@ -110,12 +110,13 @@ Page({
     // 4.监听播放的进度
     if (this.data.isFirstPlay) {
       this.data.isFirstPlay = false
-      const throttleUpdateProgress = _throttle(this.updateProgress, 500, { leading: false })
+      const throttleUpdateProgress = _throttle(this.updateProgress, 500)
 
       audioContext.onTimeUpdate(() => {
         // 1.更新歌曲的进度
         //  当我们在通过滑块拖动或者点击滑块改变歌曲进度时，虽然通过audioContext.seek()设置了audioContext的currentTime,
-        //  但是在改变完后马上去获取audioContext.currentTime依然可能会在原来的地方，大概500ms内，偶尔触发。需要通过字段限制先不要自动更新进度
+        //  但是在改变完后马上去获取audioContext.currentTime依然可能会在原来的地方，大概500ms内，偶尔触发。需要通过字段限制先不要自动更新进度，
+        //  isSliderChanging表示正在滑动。isWaiting表示刚点击完或者刚滑动结束，需要等待一段时间
         if (!this.data.isSliderChanging && !this.data.isWaiting) {
           throttleUpdateProgress()
         }
@@ -235,7 +236,6 @@ Page({
   },
   // 点击滑块后，改变iswaiting字段（使用防抖）
   debounceWaiting: _debounce(function(){
-    console.log('isWaiting')
     this.data.isWaiting = false
   },1000),
   // 点击滑块变化或拖动结束
@@ -274,6 +274,13 @@ Page({
         url: '/pages/main-music/main-music',
       })
     }
+  },
+  // 点击图片，预览
+  showImg(){
+    if(!this.data.currentSong.al) return
+    // wx.previewImage({
+    //   urls: [this.data.currentSong.al.picUrl],
+    // })
   },
   // 导航切换
   onNavTabItemTap(e) {
